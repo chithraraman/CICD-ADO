e4d450f39332edad4ace2fb0f9191e00e5902492
Git-Chitra <chithumay@yahoo.com>
2024-08-05 13:02:28 +1200

    first commit form git console
    first commit form git console

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+#   backend "s3" {
+#     bucket = "ado-practice-bucket-2"
+#     key    = "ado-state-2/ado-terraform.tfstate"
+#     region = "ap-southeast-2"
+#   }
+}
---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0809dd5035d9217b8"
+  instance_type = "t2.micro"
+  count = 1
+
+  tags = {
+    Name = "HelloWorld1"
+  }
+}
+
+# resource "aws_s3_bucket" "example" {
+#   bucket = "my-tf-test-bucket"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
